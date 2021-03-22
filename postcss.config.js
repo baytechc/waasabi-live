c53e891e20811023cabf14d8ea3a0307554c3e7b
Flaki <git@flaki.hu>
2021-03-23 00:49:09 +0200

    Clean up stylesheets, make CSS brandable

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,30 @@ import pNested from 'postcss-nested';
 import pAutoprefixer from 'autoprefixer';
 import pNano from 'cssnano';
 
+import globby from 'globby';
+
+const paths = new Set();
+[
+  // Fonts
+  globby.sync('public/assets/fonts/**/*.css'),
+
+  // Base CSS
+  //globby.sync('src/css/*.css'),
+globby.sync('src/css/{typography,forms}.css'),
+
+  // Page-specific CSS
+  globby.sync('src/css/components/*.css'),
+  globby.sync('src/css/pages/*.css'),
+
+  // Style overrides
+  globby.sync('src/css/overrides/*.css'),
+globby.sync('src/css/{layout,theme}.css'),
+
+  // Branding CSS
+  globby.sync('brand/src/css/**/*.css'),
+].forEach(glob => glob.forEach(p => paths.add(p)));
+console.log(Array.from(paths));
+
 export default (ctx) => ({
   map: ctx.options.map,
   plugins: [
@@ -15,31 +39,7 @@ export default (ctx) => ({
   //TODO: check for the canonical postcss.config.js properties
   generate: {
     header: '',
-    from: [
-      // Fonts
-      'public/assets/fonts/fonts.css',
-
-      // Defaults
-      'src/css/typography.css',
-      'src/css/forms.css',
-
-      // Components
-      'src/css/c-experience-triggers.css',
-      'src/css/c-sbox-controls.css',
-      'src/css/c-stream-video.css',
-
-      // Pages
-      'src/css/pg-home.css',
-      'src/css/pg-replay.css',
-      'src/css/pg-chat.css',
-      'src/css/pg-freebies.css',
-      'src/css/style.css',
-
-      'src/css/layout.css',
-      'src/css/theme.css',
-
-      'brand/src/css/theme.css',
-    ],
+    from: Array.from(paths),
     outdir: '_site/',
     // If no outfile don't concat into single file
     outfile: '_site/style.css',
